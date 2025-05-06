b6e2b248a88f137184fc7713ed5c7076a204df24
plo <leonardopolo266@gmail.com>
2025-05-06 19:48:58 +0200

    fix

--- index.js
+++ index.js
@@ -68,5 +68,5 @@ app.get('/users', verifyFirebaseToken, async (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.SERVER_PORT}`);
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
