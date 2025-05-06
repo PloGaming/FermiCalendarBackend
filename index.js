7266d55742bc348f871ee7784af3dcb8ac2b083a
plo <leonardopolo266@gmail.com>
2025-05-06 19:36:00 +0200

    without service-provider.json

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.json()); // For parsing application/json
 
 // Initialize Firebase
-const serviceAccount = require('./service-account.json');
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
     databaseURL: process.env.FIREBASE_DB_URL // For Realtime DB
 });
 
